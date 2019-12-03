3223d67e60d5fd65e3916fba92da0a3af3a5b0ff
Essi Supponen <essi.supponen@tuni.fi>
2019-12-03 08:36:56 +0200

    Board.js: Add initial code

---
+++ gamecomponents/Board.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import Card from './Card';
+
+export default class Board extends React.Component {
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <Card/>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
