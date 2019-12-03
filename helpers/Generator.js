6a7855c88c5eafca1efdf551e198385224015c96
Essi Supponen <essi.supponen@tuni.fi>
2019-12-03 18:55:52 +0200

    Generator.js: initial generateValues and getBoardMapping code

---
+++ helpers/Generator.js
@@ -0,0 +1,14 @@
+class Generator {
+    generateValues(numberOfPairs) {
+        values = [];
+
+        return values;
+    }
+
+    getBoardMapping(numberOfPairs) {
+        rows = 0;
+        cols = 0;
+
+        return {rows: rows, cols: cols}
+    }
+}
