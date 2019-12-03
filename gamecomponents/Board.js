c5b689fbb256bd670eb9035d3d241f464d2bd3d1
Essi Supponen <essi.supponen@tuni.fi>
2019-12-03 09:28:15 +0200

    Board.js: add colums and rows

--- gamecomponents/Board.js
+++ gamecomponents/Board.js
@@ -3,21 +3,43 @@ import { StyleSheet, Text, View } from 'react-native';
 import Card from './Card';
 
 export default class Board extends React.Component {
+    constructor(props) {
+        super(props)
 
-  render() {
-    return (
-      <View style={styles.container}>
-        <Card symbol="A"/>
-      </View>
-    );
-  }
+        this.makeBoard = this.makeBoard.bind(this);
+
+        this.state = {values: ['A', 'B', 'C', 'D']};
+    }
+
+    makeBoard() {
+        return (<View style={styles.row}>
+            <View style={styles.column}>
+                <Card symbol={this.state.values[0]}/>
+                <Card symbol={this.state.values[1]}/>
+            </View>
+            <View style={styles.column}>
+                <Card symbol={this.state.values[2]}/>
+                <Card symbol={this.state.values[3]}/>
+            </View>
+        </View>);
+    }
+
+    render() {
+        return (
+            <View>
+                {this.makeBoard()}
+            </View>
+        );
+    }
 }
 
 const styles = StyleSheet.create({
-  container: {
+  row: {
     flex: 1,
-    backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  column: {
+    flexDirection: 'row',
+  }
 });
