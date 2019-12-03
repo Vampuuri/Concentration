bdde9f8904f57701c8f406d0860ceccd8c6956c8
Essi Supponen <essi.supponen@tuni.fi>
2019-12-03 08:15:54 +0200

    Card.js: add basic style

--- gamecomponents/Card.js
+++ gamecomponents/Card.js
@@ -3,6 +3,29 @@ import { StyleSheet, Text, View } from 'react-native';
 
 export default class Card extends React.Component {
     render() {
-        return (<Text>Card</Text>)
+        return (<View style={styles.container_active}><Text>Card</Text></View>);
     }
 }
+
+const styles = StyleSheet.create({
+  container_active: {
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderStyle: 'solid',
+    borderColor: 'black',
+    borderWidth: 1,
+    width: 50,
+    height: 50,
+  },
+  container_hidden: {
+    backgroundColor: 'black',
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderStyle: 'solid',
+    borderColor: 'black',
+    borderWidth: 1,
+    width: 50,
+    height: 50,
+    },
+});
