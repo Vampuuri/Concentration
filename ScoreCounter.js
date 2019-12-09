fa09c86339109aa2cae51cb8ffac4f26a95fd148
Essi Supponen <essi.supponen@tuni.fi>
2019-12-09 14:35:29 +0200

    ScoreCounter: add style for view

--- ScoreCounter.js
+++ ScoreCounter.js
@@ -6,11 +6,18 @@ export default class Board extends React.Component {
 
     render() {
         return (
-            <Board pairs={10}/>
+            <View style={styles.container}>
+                <Board pairs={9}/>
+            </View>
         );
     }
 }
 
 const styles = StyleSheet.create({
-
-});
+    container: {
+      flex: 1,
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+  });
+  
