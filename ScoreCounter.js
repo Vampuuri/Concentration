523049cc1d3a0c8eac941bcbdcaadd151edd8e10
Essi Supponen <essi.supponen@tuni.fi>
2019-12-09 16:27:55 +0200

    ScoreCounter: pass matching successful and matchingfailed to board

--- ScoreCounter.js
+++ ScoreCounter.js
@@ -6,13 +6,13 @@ export default class ScoreCounter extends React.Component {
     constructor(props) {
         super(props)
 
-        this.machingSuccessful = this.machingSuccessful.bind(this);
+        this.matchingSuccessful = this.matchingSuccessful.bind(this);
         this.matchingFailed = this.matchingFailed.bind(this);
 
         this.state = {score: 0, combo: 10}
     }
 
-    machingSuccessful() {
+    matchingSuccessful() {
         console.log("match!");
     }
 
@@ -26,7 +26,9 @@ export default class ScoreCounter extends React.Component {
                 <View style={styles.scorecontainer}>
                     <Text style={styles.score}>{this.state.score}</Text>
                 </View>
-                <Board pairs={9}/>
+                <Board pairs={9}
+                    matchingSuccessful={this.matchingSuccessful}
+                    matchingFailed={this.matchingFailed}/>
             </View>
         );
     }
