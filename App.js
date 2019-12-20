dc9a09d50449bfacdb461fa2b729392b97360bda
Essi Supponen <essi.supponen@tuni.fi>
2019-12-20 13:00:40 +0200

    App.js: Just scrap the navigation and try something else

--- App.js
+++ App.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import ScoreCounter from './ScoreCounter';
+import Menu from './Menu';
 
 export default class App extends React.Component {
 
   render() {
     return (
       <View style={styles.container}>
-        <ScoreCounter/>
+        <Menu/>
       </View>
     );
   }
