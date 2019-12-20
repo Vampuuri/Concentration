87c9cd9fdfe249a63350eca44babc5aaa84b4956
Essi Supponen <essi.supponen@tuni.fi>
2019-12-20 13:34:52 +0200

    ScoreScreen: add text and styling

--- ScoreScreen.js
+++ ScoreScreen.js
@@ -4,7 +4,19 @@ import { StyleSheet, Text, View } from 'react-native';
 export default class ScoreCounter extends React.Component {
     render() { 
         return (
-            <View><Text>Score</Text></View>
+            <View>
+                <View style={styles.titleContainer}>
+                    <Text style={styles.titleText}>You won!</Text>
+                </View>
+                <View style={styles.scoreContainer}>
+                    <Text>Your score was:</Text>
+                    <Text>0</Text>
+                </View>
+                <View style={styles.buttonContainer}>
+                    <View style={styles.button}><Text>Stop playing</Text></View>
+                    <View style={styles.button}><Text>Try again</Text></View>
+                </View>
+            </View>
         );
     }
 }
@@ -16,5 +28,26 @@ const styles = StyleSheet.create({
       flexDirection: 'column',
       justifyContent: 'center'
     },
+    titleContainer: {
+      alignItems: 'center',
+      padding: 30,
+    },
+    titleText: {
+      fontSize:20,
+    },
+    scoreContainer: {
+      alignItems: 'center',
+      padding: 20,
+    },
+    buttonContainer: {
+      alignItems: 'center',
+      flexDirection: 'row',
+    },
+    button: {
+      alignItems: 'center',
+      backgroundColor: 'lightgrey',
+      padding: 5,
+      margin: 2,
+    }
   });
   
